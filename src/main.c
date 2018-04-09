741b1e6f8d3f788bb2a4c13ea6530f473bd68f2d
tracer <tracer@tracer.org>
2018-04-09 04:00:04 -0700

    > run wallaceyin Linux ubuntu 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:46:42 UTC 2018 i686 i686 i686 GNU/Linux

--- src/main.c
+++ src/main.c
@@ -2,12 +2,12 @@
 #include <amdev.h>
 //#include <ylib.h>
 // TODO: implement necessary libraries
-int printf(const char *fmt, ...) {
+/*int printf(const char *fmt, ...) {
   for (; *fmt; fmt++) {
     _putc(*fmt);
   }
   return 0;
-}
+}*/
 
 static void input_test(_Device *dev);
 static void timer_test(_Device *dev);
