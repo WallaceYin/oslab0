34078141acc389723e7951eda8a7e3f54c753a05
tracer <tracer@tracer.org>
2018-04-09 19:52:17 -0700

    > run wallaceyin Linux ubuntu 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:46:42 UTC 2018 i686 i686 i686 GNU/Linux

---
+++ src/device.c
@@ -0,0 +1,48 @@
+#include <device.h>
+
+uint32_t up_time(void)
+{
+	for (int n = 1;; n++)
+	{
+		_Device *dev = _device(n);
+		if (!dev) break;
+		if (dev->id == _DEV_TIMER)
+		{
+			_UptimeReg uptime;
+			uint32_t t0;
+			dev->read(_DEVREG_TIMER_UPTIME, &uptime, sizeof(uptime));
+			t0 = uptime.lo;
+			return t0;
+		}
+		else
+			continue;
+	}
+}
+
+int read_key(void)
+{
+	//TODO:
+	return 1;
+}
+
+void draw_rect(uint32_t *pixels, int x, int y, int w, int h)
+{
+	//TODO: draw pixels
+}
+
+void draw_sync(void)
+{
+	//TODO
+}
+
+int screen_width(void)
+{
+	//TODO: get the screen width
+	return 1;
+}
+
+int screen_height(void)
+{
+	//TODO: get the screen height
+	return 1;
+}
