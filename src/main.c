82aaadca4705e49d0bf2efb4c7d3cb5af53f115a
tracer <tracer@tracer.org>
2018-05-07 03:14:28 -0700

    > run wallaceyin Linux ubuntu 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:55 UTC 2018 i686 i686 i686 GNU/Linux

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ int main() {
   if (_ioe_init() != 0) _halt(1);
 #ifdef TEST
   printf("_heap = [%08x, %08x)\n", _heap.start, _heap.end);
-	int *p = _head.start;
+	int *p = _heap.start;
 	printf("duiqui: %d\n", *p);
   for (int n = 1; ; n++) {
     _Device *dev = _device(n);
